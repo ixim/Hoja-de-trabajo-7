91a20de64778de4eac543a7929681d312867a4a8
ixim <ixim_21@hotmail.com>
2014-09-08 23:56:01 -0600

    modificando

--- src/diccionario/BinaryTree.java
+++ src/diccionario/BinaryTree.java
@@ -1,70 +1,98 @@
 
 package diccionario;
+import java.util.Iterator;
 
 public class BinaryTree <E>
 {
-    protected E val; // value associated with node
-	protected BinaryTree<E> parent; // parent of node
-	protected BinaryTree<E> left, right; // children of node
+    protected E val; // value associated with node 
+    protected BinaryTree<E> parent; // parent of node
+    protected BinaryTree<E> left, right; // children of node
 	
 	public BinaryTree()
 	// post: constructor that generates an empty node
 	{
-		val = null;
-		parent = null; left = right = this;
+            val = null;
+            parent = null;
+            left = right = null;//MODIFICADO
 	}
 	public BinaryTree(E value)
 	// post: returns a tree referencing value and two empty subtrees
 	{
-		val = value;
-		right = left = new BinaryTree<E>();
-		setLeft(left);
-		setRight(right);
+            val = value;
+            right = left = new BinaryTree<E>();
+            setLeft(left);
+            setRight(right);
 	}
 	public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right)
 	// post: returns a tree referencing value and two subtrees
 	{
-		val = value;
-		if (left == null) 
-                { 
-                    left = new BinaryTree<E>(); 
-                }
-		setLeft(left);
-		if (right == null)
-                {
-                    right = new BinaryTree<E>(); 
-                }
-		setRight(right);
+            val = value;
+            if (left == null) { left = new BinaryTree<E>(); }
+            setLeft(left);
+            if (right == null){ right = new BinaryTree<E>(); }
+            setRight(right);
 	}
 	public BinaryTree<E> left()
 	// post: returns reference to (possibly empty) left subtree
 	// post: returns reference to (possibly empty) left subtree
 	{
-		return left;
+            return left;
 	}
+        public BinaryTree<E> right()
+        {
+            return right;
+        }
 	public BinaryTree<E> parent()
+        {
+            return parent;
+        }
 	// post: returns reference to parent node, or null
 	
 	public void setLeft(BinaryTree<E> newLeft)
 	// post: sets left subtree to newLeft
 	// re-parents newLeft if not null
 	{
-		if (isEmpty()) return;
-		if (left != null && left.parent() == this) left.setParent(null);
-		left = newLeft;
-		left.setParent(this);
+            if (isEmpty()) return;
+            if (left != null && left.parent() == this) left.setParent(null);
+            left = newLeft;
+            left.setParent(this);
+	}
+        public void setRight(BinaryTree<E> newRight)
+	// post: sets left subtree to newLeft
+	// re-parents newLeft if not null
+	{
+            if (isEmpty()) return;
+            if (right != null && right.parent() == this) right.setParent(null);
+            right = newRight;
+            right.setParent(this);
 	}
 	protected void setParent(BinaryTree<E> newParent)
 	// post: re-parents this node to parent reference, or null
 	{
-		if (!isEmpty()) {
-		parent = newParent;
-		}
+            if (!isEmpty()) 
+            {
+                parent = newParent;
+            }
 	}
 	public Iterator<E> iterator()
 	// post: returns an in-order iterator of the elements
+        {
+            return inorderIterator();
+        }
+        public AbstractIterator<E> inorderIterator()
+        {
+            return  new BTInorderIterator<E>(this);
+        }
+        public boolean isEmpty()
+        {
+            return val == null;
+        }
 	public boolean isLeftChild()
 	// post: returns true if this is a left child of parent
+        {
+            if (parent() == null) return false;
+            return this == parent().left();
+        }
 	public E value()
 	// post: returns value associated with this node
 	{
