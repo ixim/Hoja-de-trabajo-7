55040d753088bbce0643b82f440972cf8abe1039
ixim <ixim_21@hotmail.com>
2014-09-10 08:51:33 -0600

    modificando

--- src/diccionario/Diccionario.java
+++ src/diccionario/Diccionario.java
@@ -1,10 +1,27 @@
 
 package diccionario;
+import java.util.Scanner;
+import java.io.*;
 
 public class Diccionario 
 {
     public static void main(String[] args) 
     {
-        
+        try
+        {
+            FileReader archivo = new FileReader("diccionario.txt"); //Abrir el fichero indicado en la variable 
+            int caracter1 = 0;
+            do
+            {
+                caracter1 = archivo.read(); //Leer el primer carácter	
+            }
+            while(caracter1 != 0); //Se recorre el fichero hasta encontrar el carácter -1 que marca el final del fichero
+				
+            archivo.close(); //Cerrar el fichero
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error de lectura del fichero");
+        }
     }
 }
