0ea1493d2edca6de1f3a1d3424f6085cb245950a
ixim <ixim_21@hotmail.com>
2014-09-10 08:52:22 -0600

    modificando

--- src/diccionario/BinaryTree.java
+++ src/diccionario/BinaryTree.java
@@ -1,106 +1,87 @@
 
 package diccionario;
-import java.util.Iterator;
 
-public class BinaryTree <E>
+public class BinaryTree<E>
 {
-    protected E val; // value associated with node 
-    protected BinaryTree<E> parent; // parent of node
-    protected BinaryTree<E> left, right; // children of node
-	
-	public BinaryTree()
-	// post: constructor that generates an empty node
-	{
-            val = null;
-            parent = null;
-            left = right = null;//MODIFICADO
-	}
-	public BinaryTree(E value)
-	// post: returns a tree referencing value and two empty subtrees
-	{
-            val = value;
-            right = left = new BinaryTree<E>();
-            setLeft(left);
-            setRight(right);
-	}
-	public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right)
-	// post: returns a tree referencing value and two subtrees
-	{
-            val = value;
-            if (left == null) { left = new BinaryTree<E>(); }
-            setLeft(left);
-            if (right == null){ right = new BinaryTree<E>(); }
-            setRight(right);
-	}
-	public BinaryTree<E> left()
-	// post: returns reference to (possibly empty) left subtree
-	// post: returns reference to (possibly empty) left subtree
-	{
-            return left;
-	}
-        public BinaryTree<E> right()
+    class Nodo
+    {
+        int info;
+        Nodo izq;
+        Nodo der;
+    }
+    Nodo raiz;
+
+    public BinaryTree()
+    {
+        raiz=null;
+    }      
+    public void insertar (int info)
+    {
+        Nodo nuevo;
+        nuevo = new Nodo ();
+        nuevo.info = info;
+        nuevo.izq = null;
+        nuevo.der = null;
+        if (raiz == null)
+            raiz = nuevo;
+        else
         {
-            return right;
-        }
-	public BinaryTree<E> parent()
-        {
-            return parent;
-        }
-	// post: returns reference to parent node, or null
-	
-	public void setLeft(BinaryTree<E> newLeft)
-	// post: sets left subtree to newLeft
-	// re-parents newLeft if not null
-	{
-            if (isEmpty()) return;
-            if (left != null && left.parent() == this) left.setParent(null);
-            left = newLeft;
-            left.setParent(this);
-	}
-        public void setRight(BinaryTree<E> newRight)
-	// post: sets left subtree to newLeft
-	// re-parents newLeft if not null
-	{
-            if (isEmpty()) return;
-            if (right != null && right.parent() == this) right.setParent(null);
-            right = newRight;
-            right.setParent(this);
-	}
-	protected void setParent(BinaryTree<E> newParent)
-	// post: re-parents this node to parent reference, or null
-	{
-            if (!isEmpty()) 
+            Nodo anterior = null, reco;
+            reco = raiz;
+            while (reco != null)
             {
-                parent = newParent;
+                anterior = reco;
+                if (info < reco.info)
+                    reco = reco.izq;
+                else
+                    reco = reco.der;
             }
-	}
-	public Iterator<E> iterator()
-	// post: returns an in-order iterator of the elements
-        {
-            return inorderIterator();
+            if (info < anterior.info)
+                anterior.izq = nuevo;
+            else
+                anterior.der = nuevo;
         }
-        public AbstractIterator<E> inorderIterator()
+    }
+    private void imprimirPre (Nodo reco)
+    {
+        if (reco != null)
         {
-            return  new BTInorderIterator<E>(this);
+            System.out.print(reco.info + " ");
+            imprimirPre (reco.izq);
+            imprimirPre (reco.der);
         }
-        public boolean isEmpty()
+    }
+    public void imprimirPre ()
+    {
+        imprimirPre (raiz);
+        System.out.println();
+    }
+    private void imprimirEntre (Nodo reco)
+    {
+        if (reco != null)
+        {    
+            imprimirEntre (reco.izq);
+            System.out.print(reco.info + " ");
+            imprimirEntre (reco.der);
+        }
+    }
+    public void imprimirEntre ()
+    {
+        imprimirEntre (raiz);
+        System.out.println();
+    }
+    private void imprimirPost (Nodo reco)
+    {
+        if (reco != null)
         {
-            return val == null;
+            imprimirPost (reco.izq);
+            imprimirPost (reco.der);
+            System.out.print(reco.info + " ");
         }
-	public boolean isLeftChild()
-	// post: returns true if this is a left child of parent
-        {
-            if (parent() == null) return false;
-            return this == parent().left();
-        }
-	public E value()
-	// post: returns value associated with this node
-	{
-            return val;
-	}
-	public void setValue(E value)
-	// post: sets the value associated with this node
-	{
-            val = value;
-	}
+    }
+    public void imprimirPost ()
+    {
+        imprimirPost (raiz);
+        System.out.println();
+    }
 }
